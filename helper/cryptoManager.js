a70c68b4ff9a921b84d8c937ba86bfc4f2bfa73b
Andrii Dydyk <andriidydyk622@gmail.com>
2024-04-14 18:39:54 +0200

    Updated tests for Переказ з іншої картки, БВ (вручну) => Власна БВ

--- helper/cryptoManager.js
+++ helper/cryptoManager.js
@@ -1,63 +1,63 @@
-const forge = require('node-forge')
-const Redis = require('ioredis')
+const forge = require("node-forge");
+const Redis = require("ioredis");
 
-const Worker = require('./worker')
+const Worker = require("./worker");
 
 class CryptoManager extends Worker {
-  constructor () {
-    super()
+  constructor() {
+    super();
   }
 
-  async decrypt () {
-    const cryptogram = await this.getSessionValue('cryptogram')
-    const privateKey = await this.getSessionValue('clientPrivateKey')
+  async decrypt() {
+    const cryptogram = await this.getSessionValue("cryptogram");
+    const privateKey = await this.getSessionValue("clientPrivateKey");
 
-    const privateKeyObject = forge.pki.privateKeyFromPem(privateKey)
-    const decrypted = forge.util.decode64(cryptogram)
+    const privateKeyObject = forge.pki.privateKeyFromPem(privateKey);
+    const decrypted = forge.util.decode64(cryptogram);
 
-    return privateKeyObject.decrypt(decrypted, 'RSA-OAEP')
+    return privateKeyObject.decrypt(decrypted, "RSA-OAEP");
   }
 
-  async encryptAndSign (dataToEncrypt) {
-    const challange = await this.decrypt()
-    dataToEncrypt.challengePass = challange
+  async encryptAndSign(dataToEncrypt) {
+    const challange = await this.decrypt();
+    dataToEncrypt.challengePass = challange;
 
-    const serverPublicKey = await this.getSessionValue('serverPublicKey')
-    const clientPrivateKey = await this.getSessionValue('clientPrivateKey')
+    const serverPublicKey = await this.getSessionValue("serverPublicKey");
+    const clientPrivateKey = await this.getSessionValue("clientPrivateKey");
 
-    const jsonToEncrypt = JSON.stringify(dataToEncrypt)
-    const encryptedBytes = this.encrypt(jsonToEncrypt, serverPublicKey)
-    const cryptogram = forge.util.encode64(encryptedBytes)
-    const sign = this.signSha512(cryptogram, clientPrivateKey)
+    const jsonToEncrypt = JSON.stringify(dataToEncrypt);
+    const encryptedBytes = this.encrypt(jsonToEncrypt, serverPublicKey);
+    const cryptogram = forge.util.encode64(encryptedBytes);
+    const sign = this.signSha512(cryptogram, clientPrivateKey);
 
-    return { sign, cryptogram }
+    return { sign, cryptogram };
   }
 
-  encrypt (message, serverPublicKey) {
-    const publicKeyObject = forge.pki.publicKeyFromPem(serverPublicKey)
-    const encrypted = publicKeyObject.encrypt(message, 'RSA-OAEP')
+  encrypt(message, serverPublicKey) {
+    const publicKeyObject = forge.pki.publicKeyFromPem(serverPublicKey);
+    const encrypted = publicKeyObject.encrypt(message, "RSA-OAEP");
 
-    return encrypted
+    return encrypted;
   }
 
-  signSha512 (message, clientPrivateKey) {
-    const messageBytes = forge.util.decode64(message)
-    const privateKeyObject = forge.pki.privateKeyFromPem(clientPrivateKey)
+  signSha512(message, clientPrivateKey) {
+    const messageBytes = forge.util.decode64(message);
+    const privateKeyObject = forge.pki.privateKeyFromPem(clientPrivateKey);
 
-    const md = forge.md.sha512.create()
+    const md = forge.md.sha512.create();
 
-    md.update(messageBytes, 'raw')
+    md.update(messageBytes, "raw");
 
     const pss = forge.pss.create({
       md: forge.md.sha512.create(),
       mgf: forge.mgf.mgf1.create(forge.md.sha512.create()),
-      saltLength: 64
-    })
+      saltLength: 64,
+    });
 
-    const signature = privateKeyObject.sign(md, pss)
+    const signature = privateKeyObject.sign(md, pss);
 
-    return forge.util.encode64(signature)
+    return forge.util.encode64(signature);
   }
 }
 
-module.exports = CryptoManager
+module.exports = CryptoManager;
